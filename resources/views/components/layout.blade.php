6e9aa76e9129c88ced30909dc61be0d0c64e9ead
Danielab <prueba@gmail.com>
2025-04-28 19:52:51 -0500

    web 3

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav> 
+            <a href="/">inicio</a>
+            <a href="/notas">notas</a>
+            <a href="/matricula">matricula</a>
+            <a href="editar_notas">editar_notas</a>
+            
+    </nav>
+    <h1> 4to web</h1>
+</body>
+</html>
